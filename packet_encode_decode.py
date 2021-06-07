d87711ee82cbb4388b8683e2bf8d7be44d6549a6
wasge <wasge@wasge.es>
2021-06-07 22:47:52 +0200

    Add files via upload

--- packet_encode_decode.py
+++ packet_encode_decode.py
@@ -1,32 +1,102 @@
 import struct
+import json
+import sys
 
-def packet_encode(type, content):
+def str2hex(s):
+   return "".join("{:02x}".format(ord(c)) for c in s)
+
+def hex2int(hex):
+	hexcontent = str2hex(hex)
+	# Get the two bytes little endian length
+	lengtha = int(hexcontent[2:4], 16)
+	lengthb = int(hexcontent[0:2], 16)
+	# Convert it to decimal
+	length = (lengtha * 256) + lengthb
+	return length
+
+def json_execute(content):
+	content = json.loads(content)
+	for id in content:
+		if (content["id"] == "SubscriptionReply"):
+			messageEncode("FR", "Listpresets/channel\x00\x00")
+		elif (content["id"] == "SubscriptionLost"):
+			sys.exit()
+
+def getLEnlength(length):
+	length = struct.pack('<H', length)
+	first, second = struct.unpack('>BB', length)
+	return chr(first) + chr(second)
+
+def messageEncode(type, content):
 	# Header: \x55\x43\x00\x01 + Packet length in little endian
 	header = "UC\x00\x01"
 	# Packet: Type + \x68\x00\x65\x00 [+ json length in 4 bytes, little endian] + content
 	if (type == "JM"): # JSON Message
-		packet = type + "\x68\x00\x65\x00"
+		packet = type + "\x68\x00\x65\x00" # 4 unknown bytes. They sometimes change.
 		# Get the JSON length in 4 bytes, little endian and add it to the packet
 		# TODO This just gets the two first bytes and fakes the two last ones
-		jsonlenlength = struct.pack('<H', len(content))
-		first, second = struct.unpack('>BB', jsonlenlength)
-		packet +=  chr(first) + chr(second) + "\x00" + "\x00"
+		packet += getLEnlength(len(content)) + "\x00" + "\x00"
 	elif (type == "UM"): # UC Message
-		packet = type + "\x00\x00\x65\x00"
+		packet = type + "\x00\x00\x65\x00" # 4 unknown bytes. They sometimes change.
 	elif (type == "FR"): # File Request
-		packet = type + "\x6b\x00\x65\x00"
-		packet += "\x01\x00"
+		packet = type + "\x6b\x00\x65\x00" # 4 unknown bytes. They sometimes change.
+		packet += "\x01\x00" # This two bytes are sent before the content
+	elif (type == "PV"): # Parameter Value
+		packet = type + "\x6b\x00\x65\x00" # 4 unknown bytes. They sometimes change.
 	else:
-		packet = type + "\x68\x00\x65\x00"		
+		packet = type + "\x68\x00\x65\x00"
 	# Add the content to the packet
 	packet += content
 	# Get the packet length and convert it to 2 bytes, little endian (struct "<H")
-	lenlength = struct.pack('<H', len(packet))
-	first, second = struct.unpack('>BB', lenlength)
-	header += chr(first) + chr(second)
+	header += getLEnlength(len(packet))
 	return header + packet
 
-# result = packet_encode("JM", "{\"id\": \"QuerySlave\"}")
-# result = packet_encode("UM", "\xca\xc5")
+count = 0
+def messageDecode(content):
+	global count
+	header = "UC\x00\x01"
+	if (content[0:4] == header):
+		hexcontent = str2hex(content)
+		# Get the two bytes little endian length
+		lengtha = int(hexcontent[10:12], 16)
+		lengthb = int(hexcontent[8:10], 16)
+		# Convert it to decimal
+		length = (lengtha * 256) + lengthb
+		type = content[6:8]
+		if (type == "JM"): # JSON content
+			start = 16 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes + 4 json length bytes = 16
+			end = 16 + length
+			print ("Received: JSON: %s" % (content[start:end]))
+			json_execute(content[start:end])
+		elif (type == "PV"): # JSON content
+			start = 12 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes = 12
+			end = 12 + length
+			print ("Received: PV: %s" % (content[start:end]))
+		elif (type == "PL"): # JSON content
+			start = 12 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes = 12
+			end = 12 + length
+			print ("Received: PL: %s" % (content[start:end]))
+		elif (type == "CK"): # JSON content
+			start = 12 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes = 12
+			end = 12 + length
+			if (count == 1):
+				'''file = open("ck_file", "w")
+				file.write (content)
+				file.close()'''
+				print ("Received: CK.")
+			count = 1
+		elif (type == "BO"): # JSON content
+			start = 12 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes = 12
+			end = 12 + length
+			print ("Received: BO.")
+		else:
+			print ("Received: %s." % (type))
+	else:
+		#print ("UC content NOT found: %s" % (content))
+		print ("UC content NOT found.")
+
+# result = messageEncode("JM", "{\"id\": \"QuerySlave\"}")
+# result = messageEncode("UM", "\xca\xc5")
 # Print reult in hex
 # print(":".join("{:02x}".format(ord(c)) for c in result))
+#0x550x430x000x010
--- wasge-oc.py
+++ wasge-oc.py
@@ -1,5 +1,6 @@
 import socket
-from packet_encode_decode import packet_encode
+import threading
+from packet_encode_decode import messageEncode, messageDecode, getLEnlength, hex2int
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 47809
@@ -7,6 +8,8 @@ UDP_BUFFER = 1024
 TCP_BUFFER = 4096
 
 UDP_SEARCHING = True
+MIXER_CONNECTED = True
+packet_buffer = ""
 
 def udp_bind(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,28 +36,57 @@ def read_udp(data, addr):
             name = data[start:end]
             print("%s found at %s:%s" % (name,ip,port))
             UDP_SEARCHING = False
+            udp_sock.close()
             mixer_connect(ip, port)
+
+def message_search(packet): # There are multiple messages per packet
+	global packet_buffer # And messages on multiple packets, so we need a buffer
+	searching = True
+	searchPos = 0
+	packet_buffer += packet # Add the new packet to the buffer
+	while (searching == True):
+		position = packet_buffer.find("\x55\x43\x00\x01", searchPos) # Search for the header
+		if (position > -1):
+			messagePos = position # Position of the message
+			length = hex2int(packet_buffer[messagePos+4:messagePos+6]) # Length of the message
+			if (length > len(packet_buffer)): # Not enough data, keep searching
+				break
+			else: # Enough data, message complete
+				message = packet_buffer[messagePos:messagePos+6+length]
+				messageDecode(message) # Process the message
+				packet_buffer = packet_buffer[messagePos+6+length:] # Remove this chunk from the buffer
+				searchPos = 0
+			messagePos = 0
+		else:
+			searching = False
 
 def mixer_connect(ip, port):
     print("Trying to connect to %s:%s" % (ip, port))
     tcp_sock = tcp_connect(ip, port)
-    packet = packet_encode("JM", "{\"id\": \"QuerySlave\"}")
+    packet = messageEncode("JM", "{\"id\": \"QuerySlave\"}")
+    print(">>>QuerySlave")
     tcp_sock.send(packet)
     data = tcp_sock.recv(TCP_BUFFER)
-    print(data)
-    packeta = packet_encode("UM", "\xca\xc5") # UDP port on the computer to receive UDP data from the mixer
-    packetb = packet_encode("JM", "{\"id\": \"Subscribe\", \"clientName\": \"Universal Control AI\", \"clientInternalName\": \"ucapp\", \"clientType\": \"PC\", \"clientDescription\": \"WASGE OC\", \"clientIdentifier\": \"WASGE-OC\", \"clientOptions\": \"perm users levl redu\", \"clientEncoding\": 23117}")
+    messageDecode(data)
+    packeta = messageEncode("UM", "\xca\xc5") # UDP port on the computer to receive UDP data from the mixer
+    packetb = messageEncode("JM", "{\"id\": \"Subscribe\", \"clientName\": \"Universal Control AI\", \"clientInternalName\": \"ucapp\", \"clientType\": \"PC\", \"clientDescription\": \"WASGE OC\", \"clientIdentifier\": \"WASGE-OC\", \"clientOptions\": \"perm users levl redu\", \"clientEncoding\": 23117}")
+    print(">>>UM")
+    print(">>>Subscribe")
     tcp_sock.send(packeta + packetb)
-    data = tcp_sock.recv(TCP_BUFFER)
-    print(data)
-    #packet = packet_encode("FR", "Listpresets/channel\x00\x00")
+    #data = tcp_sock.recv(TCP_BUFFER)
+    #messageDecode(data)
+    #packet = messageEncode("FR", "Listpresets/channel\x00\x00")
+    #packet = messageEncode("PV", "main/ch1/volume\x00\x00\x00\x27\x57\x10\x3f")
     #tcp_sock.send(packet)
-    data = tcp_sock.recv(TCP_BUFFER)
-    #print(data)
+    while MIXER_CONNECTED == True:
+        data = tcp_sock.recv(TCP_BUFFER)
+        message_search(data)
+    print ("Exit")
     tcp_sock.close()
 
-udp_sock = udp_bind(UDP_IP, UDP_PORT)
-
-while UDP_SEARCHING:
-  data, addr = udp_sock.recvfrom(UDP_BUFFER)
-  read_udp(data, addr)
+if __name__ == "__main__":
+	udp_sock = udp_bind(UDP_IP, UDP_PORT)
+	
+	while UDP_SEARCHING:
+		data, addr = udp_sock.recvfrom(UDP_BUFFER)
+		read_udp(data, addr)
