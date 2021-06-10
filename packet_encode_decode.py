e933ff270068f6d63505f1369cf2ac7f7ef4b1da
wasge <wasge@wasge.es>
2021-06-10 15:39:12 +0200

    Started to really use the foutBytes
    I am finally fully using the fourBytes while sending messages and getting less ignored by the mixer.

--- packet_encode_decode.py
+++ packet_encode_decode.py
@@ -2,7 +2,7 @@ import struct
 import sys
 
 def str2hex(s):
-   return "".join("{:02x}".format(ord(c)) for c in s)
+	return "".join("{:02x}".format(ord(c)) for c in s)
 
 def hex2int(hex):
 	hexcontent = str2hex(hex)
@@ -23,19 +23,19 @@ def messageEncode(fourBytes, type, content):
 	header = "UC\x00\x01"
 	# Packet: Type + \x68\x00\x65\x00 [+ json length in 4 bytes, little endian] + content
 	if (type == "JM"): # JSON Message
-		packet = type + "\x68\x00\x65\x00" # 4 unknown bytes. They sometimes change.
+		packet = type + fourBytes
 		# Get the JSON length in 4 bytes, little endian and add it to the packet
 		# TODO This just gets the two first bytes and fakes the two last ones
 		packet += getLEnlength(len(content)) + "\x00" + "\x00"
 	elif (type == "UM"): # UC Message
-		packet = type + "\x00\x00\x65\x00" # 4 unknown bytes. They sometimes change.
+		packet = type + fourBytes
 	elif (type == "FR"): # File Request
-		packet = type + "\x6b\x00\x65\x00" # 4 unknown bytes. They sometimes change.
+		packet = type + fourBytes
 		packet += "\x01\x00" # This two bytes are sent before the content
 	elif (type == "PV"): # Parameter Value
-		packet = type + "\x6b\x00\x65\x00" # 4 unknown bytes. They sometimes change.
+		packet = type + fourBytes
 	else:
-		packet = type + "\x68\x00\x65\x00"
+		packet = type + fourBytes
 	# Add the content to the packet
 	packet += content
 	# Get the packet length and convert it to 2 bytes, little endian (struct "<H")
@@ -71,7 +71,7 @@ def messageDecode(content):
 			start = 12 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes = 12
 			end = 12 + length
 			if (count == 1):
-				'''file = open("ck_file", "w")
+				'''file = open("ck_file", "w") # Write the contets of to a file for debugging
 				file.write (content)
 				file.close()'''
 				print ("Received: CK.")
