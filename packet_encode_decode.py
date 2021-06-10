d1ed4ef672b4b74915320ad54aaccc9e97fe8aeb
wasge <wasge@wasge.es>
2021-06-10 19:34:05 +0200

    Fix regression
    Apparently not every fourBytes shoul be the same, some of them need to be distinct.

--- packet_encode_decode.py
+++ packet_encode_decode.py
@@ -2,7 +2,7 @@ import struct
 import sys
 
 def str2hex(s):
-	return "".join("{:02x}".format(ord(c)) for c in s)
+   return "".join("{:02x}".format(ord(c)) for c in s)
 
 def hex2int(hex):
 	hexcontent = str2hex(hex)
@@ -23,17 +23,17 @@ def messageEncode(fourBytes, type, content):
 	header = "UC\x00\x01"
 	# Packet: Type + \x68\x00\x65\x00 [+ json length in 4 bytes, little endian] + content
 	if (type == "JM"): # JSON Message
-		packet = type + fourBytes
+		packet = type + "\x68\x00\x65\x00" # 4 unknown bytes. They sometimes change.
 		# Get the JSON length in 4 bytes, little endian and add it to the packet
 		# TODO This just gets the two first bytes and fakes the two last ones
 		packet += getLEnlength(len(content)) + "\x00" + "\x00"
 	elif (type == "UM"): # UC Message
-		packet = type + fourBytes
+		packet = type + "\x00\x00\x65\x00" # 4 unknown bytes. They sometimes change.
 	elif (type == "FR"): # File Request
-		packet = type + fourBytes
+		packet = type + fourBytes # 4 unknown bytes. They sometimes change.
 		packet += "\x01\x00" # This two bytes are sent before the content
 	elif (type == "PV"): # Parameter Value
-		packet = type + fourBytes
+		packet = type + fourBytes # 4 unknown bytes. They sometimes change.
 	else:
 		packet = type + fourBytes
 	# Add the content to the packet
@@ -71,7 +71,7 @@ def messageDecode(content):
 			start = 12 # 4 first bytes + 2 little endian length bytes + 2 type bytes + 4bytes = 12
 			end = 12 + length
 			if (count == 1):
-				'''file = open("ck_file", "w") # Write the contets of to a file for debugging
+				'''file = open("ck_file", "w") # Write the contents of to a file for debugging
 				file.write (content)
 				file.close()'''
 				print ("Received: CK.")
@@ -88,9 +88,3 @@ def messageDecode(content):
 	else:
 		#print ("UC content NOT found: %s" % (content))
 		print ("UC content NOT found.")
-
-# result = messageEncode("JM", "{\"id\": \"QuerySlave\"}")
-# result = messageEncode("UM", "\xca\xc5")
-# Print reult in hex
-# print(":".join("{:02x}".format(ord(c)) for c in result))
-#0x550x430x000x010
