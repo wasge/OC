117d250b69fbba559de0d22ba26875f22e28241a
wasge <wasge@wasge.es>
2021-06-17 22:57:05 +0200

    Better and simpler handling of data
    The data analyzeOSC receives is more correct now, so it needs less processing.

--- oscInterface.py
+++ oscInterface.py
@@ -1,9 +1,8 @@
 def analyzeOSC(data):
 	start = data.find(",")
 	if start > -1:
-		addressStart = data.find("/")
-		addressEnd = data.find("\x00", addressStart)
-		address = data[addressStart:addressEnd]
+		addressEnd = data.find("\x00") # Find the end of the address
+		address = data[0:addressEnd]
 		value = data[(start + 4):]
 		value = value[3:4] + value[2:3] + value[1:2] + value[0:1] # Reverse the 4 bytes
 		return address, value
